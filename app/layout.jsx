d4c79dc7b963b3a6750c237fba5dc00d99943460
iwaldman <irvin.waldman@hotmail.com>
2024-08-28 18:16:43 -0500

    Metadata and assets

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,11 @@
 import '@/assets/styles/globals.css';
 
+export const metadata = {
+  title: 'Property Pulse',
+  keywords: 'real estate, property, home, house, apartment, rent, buy, sell',
+  description: 'Property Pulse is a real estate platform that helps you find the perfect home.',
+};
+
 const MainLayout = ({ children }) => {
   return (
     <html>
