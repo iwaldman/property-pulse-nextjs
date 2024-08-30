ef927fe8972232d89d0e1615cdd125cb7ada0fa5
iwaldman <irvin.waldman@hotmail.com>
2024-08-30 16:11:34 -0500

    Navbar component start

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,19 +1,21 @@
-import '@/assets/styles/globals.css';
+import '@/assets/styles/globals.css'
+import Navbar from '@/components/Navbar'
 
 export const metadata = {
   title: 'Property Pulse',
   keywords: 'real estate, property, home, house, apartment, rent, buy, sell',
   description: 'Property Pulse is a real estate platform that helps you find the perfect home.',
-};
+}
 
 const MainLayout = ({ children }) => {
   return (
     <html>
       <body>
+        <Navbar />
         <main>{children}</main>
       </body>
     </html>
-  );
-};
+  )
+}
 
-export default MainLayout;
+export default MainLayout
